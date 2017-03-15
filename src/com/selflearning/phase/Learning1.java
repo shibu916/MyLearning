d488790fec8ba47c9c9360fafb96d6ff744e6e81
shiva <shivasakthivel@shivas-mbp>
2017-03-16 00:30:56 +0800

    Testing

--- src/com/selflearning/phase/Learning1.java
+++ src/com/selflearning/phase/Learning1.java
@@ -5,6 +5,7 @@ public class Learning1 {
 	public static void main(String args[])
 	{
 		System.out.println("Ready to integrate git hub");
+		System.out.println("added for commit testing");
 		System.out.println("##End##");
 	}
 
